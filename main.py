e7c31f25e594f53df7b74af0e9c279c4383b2f26
Duda Oleksandr <oleksandr.duda@gmail.com>
2020-11-18 03:05:12 +0200

    Upload project

---
+++ main.py
@@ -0,0 +1,18 @@
+from flask import Flask
+from waitress import serve
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return '<h1><a href = "/api/v1/hello-world-6"> Hello Word </a></h1>'
+
+
+@app.route('/api/v1/hello-world-6')
+def run():
+    return '<h1> Hello world 6 </h1>'
+
+
+if __name__ == '__main__':
+    serve(app, port=5000, host='0.0.0.0')
