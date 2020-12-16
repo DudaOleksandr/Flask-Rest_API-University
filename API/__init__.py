17671fd33a87b4bf93ec77692eda47806ec69e19
Duda Oleksandr <oleksandr.duda@gmail.com>
2020-12-16 14:23:13 +0200

    Update to lab 7

--- API/__init__.py
+++ API/__init__.py
@@ -1,9 +1,16 @@
 from flask import Flask
+from flask_bcrypt import Bcrypt
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, scoped_session
 
+_version__ = '0.1.0'
 app = Flask(__name__)
+bcrypt = Bcrypt(app)
 
 engine = create_engine('mysql+pymysql://root@127.0.0.1:3306/lab5')
+SessionFactory = sessionmaker(bind=engine)
+Session = scoped_session(SessionFactory)
+session = Session()
 
-Session = sessionmaker(bind=engine)
+from . import urls
+from . import models
--- API/models.py
+++ API/models.py
@@ -1,9 +1,18 @@
-from API import engine
-from sqlalchemy.orm import relationship
-from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String, Boolean, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from marshmallow import Schema, fields, post_load
+
+from API import session
 
 Base = declarative_base()
+
+
+class ProductList(Base):
+    __tablename__ = 'product_list'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(32))
+    #all_products = relationship('ProductList', backref='product')
 
 
 class Purchase(Base):
@@ -12,17 +21,10 @@ class Purchase(Base):
     quantity = Column(Integer)
     userID = Column(Integer)
     shipDate = Column(String(64))
-    # test = Column(String(64))
     complete = Column(Boolean)
     bought_products = relationship('Product', backref='purchase')
     status = Column(String(32))
-
-
-# class ProductList(Base):
-#     __tablename__ = 'product_list'
-#     id = Column(Integer, primary_key=True)
-#     name = Column(String(32))
-#     all_products = relationship('Product', backref='purchase')
+    #purchase_r = relationship(ProductList, foreign_keys=[id])
 
 
 class Product(Base):
@@ -34,7 +36,7 @@ class Product(Base):
     is_bought = Column(Boolean)
     purchase_id = Column(Integer, ForeignKey('purchase.id'))
     purchase_r = relationship(Purchase, foreign_keys=[purchase_id])
-    #purchase_k = relationship(ProductList, foreign_keys=[purchase_id])
+    # purchase_k = relationship(ProductList, foreign_keys=[purchase_id])
 
 
 class User(Base):
@@ -46,4 +48,75 @@ class User(Base):
     user_status = Column(String(32))
     status = Column(String(32))
 
-# Base.metadata.create_all(engine)
+
+class UserSchema(Schema):
+    id = fields.Int()
+    username = fields.Str()
+    email = fields.Str()
+    password = fields.Str()
+    user_status = fields.Str()
+    status = fields.Str()
+
+    @post_load
+    def make_user(self, data, **kwargs):
+        return User(**data)
+
+
+class PurchaseSchema(Schema):
+    id = fields.Int()
+    quantity = fields.Int()
+    userID = fields.Int()
+    shipDate = fields.Str()
+    complete = fields.Bool()
+    bought_products = relationship('Product', backref='purchase')
+    status = fields.Str()
+
+    @post_load
+    def make_user(self, data, **kwargs):
+        return Purchase(**data)
+
+
+class ProductSchema(Schema):
+    id = fields.Int()
+    name = fields.Str()
+    status = fields.Str()
+    amount = fields.Int()
+    is_bought = fields.Bool()
+    purchase_id = Column(Integer, ForeignKey('purchase.id'))
+    purchase_r = fields.Nested(PurchaseSchema)
+
+    # purchase_k = relationship(ProductList, foreign_keys=[purchase_id])
+    @post_load
+    def make_user(self, data, **kwargs):
+        return Product(**data)
+
+
+class ProductListSchema(Schema):
+    id = fields.Int()
+    name = fields.Str()
+    all_products = fields.Nested(ProductSchema)
+
+    @post_load
+    def make_user(self, data, **kwargs):
+        return ProductList(**data)
+
+
+class Place(Base):
+    __tablename__ = "places"
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(String(32), unique=True)
+
+
+class PlaceSchema(Schema):
+    id = fields.Int()
+    name = fields.Str()
+
+    @post_load
+    def make_place(self, data, **kwargs):
+        return Place(**data)
+
+def check_None(cls, pk):
+    obj = session.query(cls).get(pk)
+    if obj is None:
+        raise Exception
+    return obj
